f9ad0dbab321abd62d9ad86795f82dcc0b303b72
Yusei Mizutani <yusei4520030401@gmail.com>
2023-01-05 22:27:30 +0900

    必要なページの追加

---
+++ frontend/src/components/pages/Calender.jsx
@@ -0,0 +1,5 @@
+import { memo } from "react";
+
+export const Calender = memo(() => {
+  return <p>カレンダーページです</p>;
+});
---
+++ frontend/src/components/pages/SignUp.jsx
@@ -0,0 +1,5 @@
+import { memo } from "react";
+
+export const SignUp = memo(() => {
+  return <p>サインアウトページです</p>;
+});
