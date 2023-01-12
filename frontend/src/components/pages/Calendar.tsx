e5a2f61b78061f46ce044756c0c34d8616f11f00
ryomaruu <120063290+ryomaruu@users.noreply.github.com>
2023-01-12 16:19:33 +0900

    Update Calendar.tsx

--- frontend/src/components/pages/Calendar.tsx
+++ frontend/src/components/pages/Calendar.tsx
@@ -1,25 +1,45 @@
-import { memo } from "react";
+import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
-export const Calendar = memo(() => {
-  return (
-    <FullCalendar
-        plugins={[ dayGridPlugin, timeGridPlugin,interactionPlugin ]}
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        }}
-        initialView="dayGridMonth"
-        events={[{ title: '正月', date: '2023-01-01' }]}
-        // dateClick={console.log("dateclick!")}
-        weekends={true}
-    />
-  );
-  
+export  class Calendar extends React.Component {
 
-  
-});
+  render(){
+    return (
+      <FullCalendar
+          plugins={[ dayGridPlugin, timeGridPlugin,interactionPlugin ]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          }}
+          events={[{title: '正月',date: '2023-01-01'}]}
+          selectable={true}
+          select={this.handleDateClick}
+          eventClick={this.eventClick}
+      />
+    );
+  }
+  handleDateClick=(arg: any) => {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = arg.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        title: title,
+        start: arg.startStr,
+        end: arg.endStr,
+        allDay: arg.allDay
+      })
+    }
+  }
+  eventClick=(eventInfo: any) =>{
+    if (confirm(`Are you sure you want to delete the event '${eventInfo.event.title}'`)) {
+      
+    }
+  }
+}
