3492f4121b814ca9cf58eb18960f1671d99d9e58
Bayashin <k21099kk@aitech.ac.jp>
2023-01-19 13:22:04 +0900

    サインイン後にユーザごとのカレンダーページへ遷移させる

--- frontend/src/hooks/auth/useSignUpUser.ts
+++ frontend/src/hooks/auth/useSignUpUser.ts
@@ -1,6 +1,7 @@
-import { useCallback } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth"
-import { auth } from "../firebase/firebase"
+import { useCallback, } from "react";
+import { createUserWithEmailAndPassword, } from "firebase/auth"
+import { auth, } from "../firebase/firebase"
+import { useNavigate, } from "react-router-dom";
 
 interface Props {
   email: string;
@@ -9,43 +10,44 @@ interface Props {
 }
 
 export const useSignUpUser = () => {
+  const navigate = useNavigate()
   const signUp = useCallback((props: Props) => {
     const { email, password, confirmationPassword } = props;
 
     // パスワードと再入力されたパスワードが違う場合
     if (password !== confirmationPassword) alert("パスワードが一致しません");
-
     // 登録
-      createUserWithEmailAndPassword(auth, email, password )
-      .then(( userCredential) => {
+    createUserWithEmailAndPassword(auth, email, password )
+      .then(( userCredential ) => {
         console.log('user created');
-        console.log(userCredential);
+        const uid = auth.currentUser?.uid
+        navigate('/'+ uid +'/calender/')
       })
       .catch((error) => {
         switch (error.code) {
           case "auth/network-request-failed":
             alert("通信がエラーになったのか、またはタイムアウトになりました。通信環境がいい所で再度やり直してください。");
-            console.log(error)
+            console.error(error)
             break;
           case "auth/weak-password":  //バリデーションでいかないようにするので、基本的にはこのコードはこない
             alert("パスワードが短すぎます。6文字以上を入力してください。");
-            console.log(error)
+            console.error(error)
             break;
           case "auth/invalid-email":  //バリデーションでいかないようにするので、基本的にはこのコードはこない
             alert("メールアドレスが正しくありません");
-            console.log(error)
+            console.error(error)
             break;
           case "auth/email-already-in-use":
             alert("メールアドレスがすでに使用されています。ログインするか別のメールアドレスで作成してください");
-            console.log(error)
+            console.error(error)
             break;
           default:  //想定外
             alert("アカウントの作成に失敗しました。通信環境がいい所で再度やり直してください。");
-            console.log(error)
+            console.error(error)
           }
       }); 
 
-  }, []);
+  }, [navigate]);
 
   return { signUp };
 };
