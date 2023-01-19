d2cf7af4c1ccc476d3b2d67aa775fbcc9795e266
Bayashin <k21099kk@aitech.ac.jp>
2023-01-19 15:36:11 +0900

    カレンダー画面からログイン画面への遷移

--- frontend/src/components/pages/Calendar.tsx
+++ frontend/src/components/pages/Calendar.tsx
@@ -1,10 +1,18 @@
 import { memo } from "react";
+import { auth } from "../../hooks/firebase/firebase";
+import { useNavigate, } from "react-router-dom";
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
 export const Calendar = memo(() => {
+  const navigate = useNavigate()
+  auth.onAuthStateChanged((user) => {
+    if (!user) {
+      navigate("/");
+    }
+  });
   return (
     <FullCalendar
         plugins={[ dayGridPlugin, timeGridPlugin,interactionPlugin ]}
