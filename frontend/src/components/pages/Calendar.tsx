89e102966f8efcf45704025899b95a4e842a514e
Bayashin <k21099kk@aitech.ac.jp>
2023-01-19 16:40:10 +0900

    ログイン後にURLでカレンダーが表示できてしまうのを修正

--- frontend/src/components/pages/Calendar.tsx
+++ frontend/src/components/pages/Calendar.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { auth } from "../../hooks/firebase/firebase";
-import { useNavigate, } from "react-router-dom";
+import { useNavigate, useLocation, } from "react-router-dom";
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
@@ -8,11 +8,17 @@ import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
 export const Calendar = memo(() => {
   const navigate = useNavigate()
+  const pathname = useLocation().pathname;
   auth.onAuthStateChanged((user) => {
     if (!user) {
       navigate("/");
+    } else if (pathname !== "/"+ user.uid +"calender") {
+        navigate("/");
+    } else {
+      console.log(user?.uid)
     }
   });
+
   return (
     <FullCalendar
         plugins={[ dayGridPlugin, timeGridPlugin,interactionPlugin ]}
